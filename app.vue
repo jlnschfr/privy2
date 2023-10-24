d6bd7bc02f187a6c59b0b8a1df621082cd3f9f83
Julian Schäfer <me@julian-schaefer.net>
2023-10-24 20:18:51 +0200

    add github login button and remove obsolete code

--- app.vue
+++ app.vue
@@ -2,6 +2,7 @@
   <div
     class="font-body font-normal text-neutral-200 bg-neutral-500 dark:text-neutral-400 dark:bg-neutral-50 min-h-screen transition duration-300"
   >
+    <AppHeader />
     <NuxtPage />
   </div>
 </template>
--- components/_Button.vue
+++ components/_Button.vue
@@ -3,8 +3,10 @@
     :type="type"
     :disabled="disabled"
     :class="{
-      'bg-primary-500 text-neutral-600 border-primary-500 dark:border-primary-500 hover:bg-primary-600 dark-hover:bg-primary-600': primary,
-      'bg-secondary-500 text-neutral-600 border-secondary-500 dark:border-secondary-500 hover:bg-secondary-600 dark-hover:bg-secondary-600': secondary,
+      'bg-primary-500 text-neutral-600 border-primary-500 dark:border-primary-500 hover:bg-primary-600 dark-hover:bg-primary-600':
+        primary,
+      'bg-secondary-500 text-neutral-600 border-secondary-500 dark:border-secondary-500 hover:bg-secondary-600 dark-hover:bg-secondary-600':
+        secondary,
       'text-opacity-50 dark:text-opacity-50 cursor-not-allowed': disabled,
     }"
     class="bg-transparent hover:bg-neutral-500 dark-hover:bg-neutral-200 text-neutral-200 dark:text-neutral-600 border-neutral-200 dark:border-neutral-400 border block px-2 md:px-4 py-0_5 privy-focus"
@@ -19,7 +21,7 @@ export default {
   props: {
     type: {
       type: String,
-      default: 'button',
+      default: "button",
       required: false,
     },
     disabled: {
@@ -38,5 +40,5 @@ export default {
       required: false,
     },
   },
-}
+};
 </script>
