5d5be48b17c518ecabd3f5a089b7a4f402cd6bc6
Julian Schäfer <me@julian-schaefer.net>
2023-10-27 12:28:12 +0200

    add TitleTextarea component

---
+++ components/_TitleTextarea.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+defineProps(["modelValue"]);
+defineEmits(["update:modelValue"]);
+</script>
+
+<template>
+  <textarea
+    :value="modelValue"
+    class="privy-focus h-5 resize-none overflow-hidden bg-transparent text-2xl font-bold"
+    placeholder="Title"
+    @input="
+      $emit('update:modelValue', ($event.target as HTMLTextAreaElement).value)
+    "
+  />
+</template>
+
+<!-- <script>
+import TextareaHeightUpdater from "@/mixins/textarea-height-updater.js";
+
+export default {
+  mixins: [TextareaHeightUpdater],
+  props: {
+    value: {
+      type: String,
+      required: false,
+      default: "",
+    },
+  },
+};
+</script> -->
