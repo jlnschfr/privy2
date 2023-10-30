f350c5b1581955285efc674eb04310b5f6073e1b
Julian Schäfer <me@julian-schaefer.net>
2023-10-30 22:50:58 +0100

    clean up and use consistent naming

--- components/_ResponsiveTextarea.vue
+++ components/_ResponsiveTextarea.vue
@@ -12,7 +12,7 @@ const emit = defineEmits(["update:modelValue"]);
 const textarea: Ref<HTMLTextAreaElement> = ref(null);
 const { updateTextareaHeight } = useTextareaHeight(textarea);
 
-const handleInput = debounce((e) => {
+const onInput = debounce((e) => {
   updateTextareaHeight();
   emit("update:modelValue", (e.target as HTMLTextAreaElement).value);
 }, 500);
@@ -24,6 +24,6 @@ const handleInput = debounce((e) => {
     :value="modelValue"
     class="h-5 resize-none overflow-hidden bg-transparent text-2xl font-bold"
     placeholder="Title"
-    @input="handleInput"
+    @input="onInput"
   />
 </template>
