892575d40083fc3e3e45e06586a9b507af0697ef
Julian Schäfer <me@julian-schaefer.net>
2023-11-02 19:07:21 +0100

    add and apply layouts

--- app.vue
+++ app.vue
@@ -2,25 +2,11 @@
 const user = useSupabaseUser();
 const noteStore = useNoteStore();
 
-const showDrawer: Ref<boolean> = ref(false);
-
 if (user.value) {
   await noteStore.fetchAll();
 }
 </script>
 
 <template>
-  <div
-    class="min-h-screen bg-neutral-500 font-body font-normal text-neutral-200 transition duration-300 dark:bg-neutral-50 dark:text-neutral-400"
-  >
-    <PrivyHeader @toggle-drawer="showDrawer = !showDrawer" />
-    <PrivyDrawer
-      :is-active="showDrawer"
-      @toggle-drawer="showDrawer = !showDrawer"
-    />
-    <main class="relative p-4vw md:pl-app">
-      <NuxtPage />
-    </main>
-    <Snackbar />
-  </div>
+  <NuxtPage />
 </template>
