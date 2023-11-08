b2654264d591e85f5d8de0e9469ef52a9cf85704
Julian Schäfer <me@julian-schaefer.net>
2023-11-08 18:36:33 +0100

    move staggered transition to note overview

--- components/PrivyNoteOverview.vue
+++ components/PrivyNoteOverview.vue
@@ -3,22 +3,42 @@ const noteStore = useNoteStore();
 const queryParams = useQueryParams();
 
 const { activeTag } = queryParams;
+
 const notes: ComputedRef<Note[]> = computed(() =>
   noteStore.getFiltered(activeTag.value),
 );
+
+function getDelay(index: number): string {
+  return index < 6 ? `${index * 25}ms` : `${6 * 25}ms`;
+}
 </script>
 
 <template>
   <div class="relative">
-    <div
+    <TransitionGroup
+      tag="div"
+      name="staggered-transition"
+      mode="out-in"
       class="mb-6 grid grid-cols-1 items-center gap-4vw pb-4vw md:grid-cols-2 md:pb-0 lg:grid-cols-3 lg:gap-2vw"
     >
       <PrivyNoteTeaser
         v-for="(note, key) in notes"
         :key="key"
         :note="note"
-        :data-index="key"
+        :style="{ transitionDelay: getDelay(key) }"
       />
-    </div>
+    </TransitionGroup>
   </div>
 </template>
+
+<style>
+.staggered-transition-enter-active,
+.staggered-transition-leave-active {
+  transition: all 300ms ease;
+}
+.staggered-transition-enter-from,
+.staggered-transition-leave-to {
+  opacity: 0;
+  transform: translateY(-50px);
+}
+</style>
