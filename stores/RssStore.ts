fd782227c28d64f6303376e3cc762fd711a3b7e7
Julian Schäfer <me@julian-schaefer.net>
2023-12-12 23:53:34 +0100

    try rss parser with netlify function

--- stores/RssStore.ts
+++ stores/RssStore.ts
@@ -1,16 +1,18 @@
 import { defineStore } from "pinia";
-import Parser from "rss-parser";
 
 export const useRssStore = defineStore("RssStore", () => {
   const feeds: Ref<Feed[]> = ref([]);
-  const parser: Parser = new Parser({
-    defaultRSS: 2.0,
-  });
 
   const fetchAll = async () => {
-    // const feed = await parser.parseURL("https://www.reddit.com/.rss");
-    // console.log(feed.title);
-    // console.log(feed.items);
+    try {
+      const functionUrl: string = `/.netlify/functions/rss}`;
+      const functionResponse: Response = await fetch(functionUrl);
+      const result = await functionResponse.json();
+
+      console.log(result);
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   return {
---
+++ types/rss.d.ts
@@ -0,0 +1,4 @@
+declare module "rss-parser/dist/rss-parser.min.js";
+{
+  export * from "rss-parser";
+}
