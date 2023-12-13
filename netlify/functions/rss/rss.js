cbb0b20db3b094181e4de1a83ddb405484ff5d4a
Julian Schäfer <me@julian-schaefer.net>
2023-12-13 13:18:37 +0100

    netlify functions test

--- netlify/functions/rss/rss.js
+++ netlify/functions/rss/rss.js
@@ -1,10 +1,14 @@
-import RSSParser from "rss-parser/dist/rss-parser.min.js";
+// import RSSParser from "rss-parser/dist/rss-parser.min.js";
+import Parser from "rss-parser";
 
 export const handler = async () => {
-  const parser = new RSSParser();
-  const feed = await parser.parseURL(
-    "https://www.tagesschau.de/inland/gesellschaft/index~rss2.xml",
-  );
+  const parser = new Parser();
+  const feed = await parser.parseURL("https://www.reddit.com/.rss");
+  console.log(feed.title); // feed will have a `foo` property, type as a string
+
+  feed.items.forEach((item) => {
+    console.log(item.title + ":" + item.link); // item will have a `bar` property type as a number
+  });
 
   return {
     statusCode: 200,
