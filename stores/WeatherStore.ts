8acc8a866aabcfa358cc86f1e6b7bc1b6a0d93f9
Julian Schäfer <me@julian-schaefer.net>
2024-02-04 19:47:53 +0100

    clean up

--- stores/WeatherStore.ts
+++ stores/WeatherStore.ts
@@ -2,7 +2,9 @@ import { defineStore } from "pinia";
 import { useLocalStorage } from "@vueuse/core";
 
 export const useWeatherStore = defineStore("WeatherStore", () => {
+  const DURATION = 1800000;
   const user = useSupabaseUser();
+
   const locationStore = useLocationStore();
   const weather: Ref<PrivyWeather> = useLocalStorage(
     `weather-${user?.value?.id}`,
@@ -13,31 +15,28 @@ export const useWeatherStore = defineStore("WeatherStore", () => {
     await fetchWeather(state.location);
   });
 
+  // Does a weather object for the current location exist?
   const isValid: ComputedRef<boolean> = computed(
     () =>
       weather.value?.location?.lat === locationStore.location.lat &&
       weather.value?.location?.long === locationStore.location.long,
   );
 
-  const isOlderThanHalfAnHour: ComputedRef<boolean> = computed(
-    () => Date.now() - weather.value?.timestamp > 1800000,
+  // Is the weather out of date and older than 30 min?
+  const isOutOfDate: ComputedRef<boolean> = computed(
+    () => Date.now() - weather.value?.timestamp > DURATION,
   );
 
   const init = async () => {
     if (locationStore.isEmpty) return;
 
-    if (isValid.value) {
-      if (isOlderThanHalfAnHour.value) {
-        await fetchWeather(locationStore.location);
-      }
-    } else {
+    if (!isValid.value || isOutOfDate.value) {
       await fetchWeather(locationStore.location);
     }
-
-    // Todo: 30min interval
   };
 
   const fetchWeather = async (location: PrivyLocation) => {
+    console.log("fetching weather");
     const url: URL = new URL("https://api.weatherapi.com/v1/current.json");
     url.searchParams.set("aqi", "no");
     url.searchParams.set("key", "8e53893c18944438bdf142917230811");
